1e637ad391f644dc7fc1f255d74ebb9fe0766c48
Xdmony <jerkyadmon@gmail.com>
2022-03-13 18:55:10 +0800

    #2022-03-13 springboot-demo

---
+++ src/main/java/com/yamabuki/springbootdemo/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.yamabuki.springbootdemo.controller;
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yamabuki.springbootdemo.common.Result;
+import com.yamabuki.springbootdemo.entity.User;
+import com.yamabuki.springbootdemo.mapper.UserMapper;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    UserMapper userMapper;
+
+    @PostMapping
+    public Result<?> save(@RequestBody User user){
+        if(StringUtils.isEmpty(user.getPassword())){
+            user.setPassword("123456");
+        }
+        userMapper.insert(user);
+        return Result.success();
+    }
+
+    @PutMapping
+    public Result<?> update(@RequestBody User user){
+        userMapper.updateById(user);
+        return Result.success();
+    }
+
+    @GetMapping
+    public Result<?> findPage(@RequestParam(defaultValue = "1") Integer pageNum,
+                              @RequestParam(defaultValue = "10") Integer pageSize,
+                              @RequestParam(defaultValue = "") String search){
+
+        LambdaQueryWrapper<User> wrapper = Wrappers.<User>lambdaQuery();
+        if(StrUtil.isNotBlank(search)){
+            wrapper.like(User::getNickName, search);
+        }
+        Page<User> page = userMapper.selectPage(new Page<>(pageNum, pageSize), wrapper);
+        return Result.success(page);
+    }
+}
---
+++ src/main/java/com/yamabuki/springbootdemo/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.yamabuki.springbootdemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yamabuki.springbootdemo.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
